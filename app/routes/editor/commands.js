036ef03e7441994323bd7d840353dd052d613ab3
josereitech <jose.rodrigues@glasscad.com.br>
2022-01-24 19:03:50 -0300

    update

--- app/routes/editor/commands.js
+++ app/routes/editor/commands.js
@@ -1,15 +1,18 @@
-const express = require("express")
+const express = require("express");
+const multer = require('multer');
+const upload = multer();
 
 const router = new (express).Router();
 
-router.get('/editor/commands/addNode', (req, res) => {
+router.get('/editor/commands/addNode', upload.none(), (req, res) => {
     console.log('received data');
+    const {x, y, width, height} = req.body;
     const io = req.app.get('io');
     //const sockets = req.app.get('sockets');
     //const thisSocketId = sockets[req.UsuarioId];
     //const socketInstance = io.to(thisSocketId);
     //socketInstance.emit('produtoSyncProgress', 0);
-    io.emit('command','addNode')
+    io.emit('command','addNode',x, y, width, height)
     res.status(200).send({
         status: 1,
     });
