b45cc2558c1e57b5626673af1a3849fe578245ff
josereitech <jose.rodrigues@glasscad.com.br>
2022-01-24 19:51:40 -0300

    udpate

--- app/routes/editor/commands.js
+++ app/routes/editor/commands.js
@@ -14,7 +14,7 @@ router.get('/editor/commands/addNode', (req, res) => {
     res.status(200).send({status: 1});
 });
 
-router.get('/editor/commands/changeSize', (req, res) => {
+router.get('/editor/commands/updateNode', (req, res) => {
     const formData = req.query;
     
     const io = req.app.get('io');
@@ -22,7 +22,7 @@ router.get('/editor/commands/changeSize', (req, res) => {
     //const thisSocketId = sockets[req.UsuarioId];
     //const socketInstance = io.to(thisSocketId);
     //socketInstance.emit('produtoSyncProgress', 0);
-    io.emit('command','changeSize',formData)
+    io.emit('command','updateNode',formData)
     res.status(200).send({status: 1});
 });
 
