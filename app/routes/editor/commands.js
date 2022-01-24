97da6dbd6f94c9a83f993286a91bd24a9fc868c2
josereitech <jose.rodrigues@glasscad.com.br>
2022-01-24 19:21:50 -0300

    update

--- app/routes/editor/commands.js
+++ app/routes/editor/commands.js
@@ -14,7 +14,7 @@ router.get('/editor/commands/addNode', upload.none(), (req, res) => {
     //const thisSocketId = sockets[req.UsuarioId];
     //const socketInstance = io.to(thisSocketId);
     //socketInstance.emit('produtoSyncProgress', 0);
-    io.emit('command','addNode')
+    io.emit('command','addNode',formData)
     res.status(200).send(formData);
 });
 
