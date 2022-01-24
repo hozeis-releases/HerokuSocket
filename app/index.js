6c082869384a787e8d67f08672660f45c18715c6
josereitech <jose.rodrigues@glasscad.com.br>
2022-01-24 16:36:21 -0300

    updated

---
+++ app/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// The io instance is set in Express so it can be grabbed in a route
+app.set('io', io);
+
+require('./io')(io, app);
+
+// simple route
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+});
+
+// set port, listen for requests
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ app/io.js
@@ -0,0 +1,22 @@
+module.exports = function (io, app) {
+  io.on("connection", (socket) => {
+    console.log(`Client connected: ${socket.id}`, "socketio");
+    // either with send()
+    socket.send("Hello!");
+
+    // or with emit() and custom event names
+    socket.emit("greetings", "Hey!", { ms: "jane" }, Buffer.from([4, 3, 3, 1]));
+
+    // handle the event sent with socket.send()
+    socket.on("message", (data) => {
+      console.log(data);
+    });
+
+    // handle the event sent with socket.emit()
+    socket.on("salutations", (elem1, elem2, elem3) => {
+      console.log(elem1, elem2, elem3);
+    });
+  });
+
+  //setInterval(() => io.emit("time", new Date().toTimeString()), 30000);
+};
